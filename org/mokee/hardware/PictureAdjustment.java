d3439b533a7e68548d0d15a0d49e9ce3ebdbd01c
martincz <martincz@mokeedev.com>
2017-12-28 04:04:27 +0800

    mkhw: mokee-ify

--- org/mokee/hardware/PictureAdjustment.java
+++ org/mokee/hardware/PictureAdjustment.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2016 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +15,11 @@
  * limitations under the License.
  */
 
-package org.lineageos.hardware;
+package org.mokee.hardware;
 
 import android.util.Range;
 
-import lineageos.hardware.HSIC;
+import mokee.hardware.HSIC;
 
 /**
  * Picture adjustment support
